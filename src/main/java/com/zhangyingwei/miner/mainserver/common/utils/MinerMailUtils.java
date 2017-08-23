0a8410fd8ad61b7a961108fffdca2a9ae1c00b53
treeholeb@gmail.com <treeholeb@gmail.com>
2017-08-23 21:59:29 +0800

    统计订阅对应的content那里逻辑是错的，明天再说吧

--- src/main/java/com/zhangyingwei/miner/mainserver/common/utils/MinerMailUtils.java
+++ src/main/java/com/zhangyingwei/miner/mainserver/common/utils/MinerMailUtils.java
@@ -5,7 +5,7 @@ import com.zhangyingwei.miner.mainserver.repertory.model.Subscribe;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Properties;
+import java.util.*;
 
 /**
  * @author: zhangyw
@@ -29,8 +29,36 @@ public class MinerMailUtils {
         return config.getProperty("miner.mail.title");
     }
 
-    public static String content(Subscribe sub, Content content) {
-
+    public static String content(Subscribe sub, List<Content> contents) {
+        Map<String, List<Content>> map = new HashMap<String,List<Content>>();
+        contents.stream().map(content -> Tuple.mk(content.getTopic(),content)).forEach(tuple -> {
+            if(map.containsKey(tuple._(0))){
+                map.get(tuple._(0)).add((Content) tuple._(1));
+            }else{
+                List<Content> list = new ArrayList<Content>();
+                map.put((String) tuple._(0), list);
+            }
+        });
         return null;
+    }
+
+    static class Tuple<A> {
+
+        public static <A> Tuple mk(A ... args){
+            return new Tuple(args);
+        }
+
+        private A[] items;
+
+        private Tuple(A[] items) {
+            this.items = items;
+        }
+
+        public A _(int index){
+            if(index < 0 || items == null || index > items.length-1){
+                return null;
+            }
+            return items[index];
+        }
     }
 }
