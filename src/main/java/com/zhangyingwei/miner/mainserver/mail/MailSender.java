e40d16fe1d83093b3514036e7f3a9ae115080487
zhangyw <zhangyw001@gmail.com>
2017-09-18 11:41:39 +0800

    添加了发送邮件随机睡眠机制

--- src/main/java/com/zhangyingwei/miner/mainserver/mail/MailSender.java
+++ src/main/java/com/zhangyingwei/miner/mainserver/mail/MailSender.java
@@ -12,6 +12,8 @@ import org.apache.log4j.Logger;
 import javax.mail.MessagingException;
 import java.io.UnsupportedEncodingException;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by zhangyw on 2017/8/24.
@@ -27,19 +29,28 @@ public class MailSender {
         this.mailController = new MailController();
     }
 
+    /**
+     * 每次发送之前随机等待 1-10 秒
+     * 主要是为了应对各种不可预见的封号情况
+     * @throws MinerMailServerException
+     */
     public void send() throws MinerMailServerException {
         List<MailInfo> mails = this.mailController.getMainInfos();
         logger.info("mail size:" + mails.size());
+        Random random = new Random();
         mails.forEach(mail -> {
             try {
                 new Smail(new SmailConfig().setStarttls(true).setNikename("Miner"))
                         .auth(username, password)
                         .to(mail.getTo())
                         .send(mail.getTitle(), mail.getContent());
+                TimeUnit.SECONDS.sleep(random.nextInt(10));
                 logger.info("send: "+ mail);
             } catch (UnsupportedEncodingException | MessagingException | SmailException e) {
 //                e.printStackTrace();
                 logger.error(e.getMessage());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         });
     }
